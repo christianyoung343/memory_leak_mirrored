24b13341a8526ac9c8c350d2b3a96a49451c68f8
Mbauer-6 <mbauer1015@gmail.com>
2022-02-12 20:20:10 -0600

    added models for database

---
+++ memory_leak/src/models/question.ts
@@ -0,0 +1,22 @@
+export class Question {
+
+    public uid?: string;
+    public title: string;
+    public body: string;
+    public anonymous: boolean;
+    public comments: Array<[string,string]>[];
+    public askerID: string; //ID of user that asked the question
+    public acceptedAnswerID?: string; 
+    public flag: number;
+    
+     constructor(questionObj:any) {
+        this.uid = questionObj.uid;
+        this.title = questionObj.title;
+        this.body = questionObj.body;
+        this.anonymous = questionObj.anonymous;
+        this.comments = questionObj.comments;
+        this.askerID = questionObj.askerID;
+        this.acceptedAnswerID = questionObj.acceptedAnswerID;
+        this.flag = questionObj.flag
+    }
+}
