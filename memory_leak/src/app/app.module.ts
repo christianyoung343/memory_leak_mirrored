9d14fb65d401b9a8f16e9c0a754e61b1220f3f43
christianyoung343 <csy5@students.uwf.edu>
2022-02-20 04:55:25 +0000

    form is now submittable; todo askerID + push form data to database

--- memory_leak/src/app/app.module.ts
+++ memory_leak/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -56,6 +57,8 @@ const firebaseConfig = {
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [{
     provide: QUESTION_CREATION_SERVICE,
--- memory_leak/src/app/containers/question-creation/question-creation-parent/question-creation-parent.component.ts
+++ memory_leak/src/app/containers/question-creation/question-creation-parent/question-creation-parent.component.ts
@@ -1,6 +1,5 @@
-//TODO: fix NullInjectionError somehow
-
 import { Component, InjectionToken, Injector, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { QuestionCreationService } from 'src/app/services/question-creation.service';
 import { Question } from 'src/models/question';
 
@@ -12,15 +11,25 @@ import { Question } from 'src/models/question';
 export class QuestionCreationParentComponent implements OnInit {
   @Input()
   source!: InjectionToken<QuestionCreationService>;
-
   private qcs!: QuestionCreationService;
+  questionForm: any;
 
   constructor(private injector: Injector) {
-     
+     this.questionForm = new FormGroup ({
+      title: new FormControl(""),
+      body: new FormControl(""),
+      anonymous: new FormControl(false)
+    })
   }
 
   ngOnInit() {
     this.qcs = this.injector.get<QuestionCreationService>(this.source);
+    
+  }
+
+  onSubmit(data: any) {
+    //TODO pass in data to addQuestion + get askerID
+    console.log("title: " + data.title + "\nbody: " + data.body + "\nanonymous?: " + data.anonymous);
   }
 
   addQuestion(question: Question) {
