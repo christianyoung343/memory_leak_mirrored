40d922b7d52da23a29c74d365b7b651eb890d2bb
Mbauer-6 <mbauer1015@gmail.com>
2022-04-07 21:18:41 -0500

    intial start to the profile page

--- memory_leak/src/app/containers/profile/profile-parent/profile-parent.component.ts
+++ memory_leak/src/app/containers/profile/profile-parent/profile-parent.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { QuestionService } from 'src/app/services/question.service';
+import { UserService } from 'src/app/services/user.service';
+import { Question } from 'src/models/question';
+import { User } from 'src/models/user';
 
 @Component({
-  selector: 'app-profile-parent',
-  templateUrl: './profile-parent.component.html',
-  styleUrls: ['./profile-parent.component.css']
+    selector: 'app-profile-parent',
+    templateUrl: './profile-parent.component.html',
+    styleUrls: ['./profile-parent.component.css']
 })
 export class ProfileParentComponent implements OnInit {
+    @Input() public user!: User;
+    @Input() public questions: Question[] = [];
+    @Input() public userFirstLetter!: string;
 
-  constructor() { }
+    constructor(private us: UserService, private qs: QuestionService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.us.getUser().subscribe(user => {
+            if (user) {
+                this.user = user;
+                this.userFirstLetter = this.user.displayName.charAt(0);
+            }
+        });
+
+        this.qs.getQuestions().subscribe(questions => {
+            questions.forEach(q => {
+               // console.log(this.user.uid);
+                if (q.askerID == this.user.uid) {
+                    this.questions.push(q);
+                }
+            })
+        });
+    }
 
 }
+
