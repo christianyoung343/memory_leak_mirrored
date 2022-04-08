e92a172c7d41c22ee04e4904929e768dd8cb2d85
christianyoung343 <csy5@students.uwf.edu>
2022-04-08 01:35:10 +0100

    flagged questions show up on admin page

--- memory_leak/src/app/containers/admin/admin-parent/admin-parent.component.ts
+++ memory_leak/src/app/containers/admin/admin-parent/admin-parent.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { QuestionService } from 'src/app/services/question.service'
+import { UserService } from 'src/app/services/user.service'
+
+import { Question } from 'src/models/question'
+import { User } from 'src/models/user';
 
 @Component({
-  selector: 'app-admin-parent',
-  templateUrl: './admin-parent.component.html',
-  styleUrls: ['./admin-parent.component.css']
+	selector: 'app-admin-parent',
+	templateUrl: './admin-parent.component.html',
+	styleUrls: ['./admin-parent.component.css']
 })
 export class AdminParentComponent implements OnInit {
+	@Input() userService!: UserService;
+	
+	user$: User | null | undefined;
 
-  constructor() { }
+	allQuestions: Question[] = [];
+	@Input() flaggedQuestions: Question[] = [];
 
-  ngOnInit(): void {
-  }
+	constructor(private userServ: UserService, private questionService: QuestionService) { }
+
+	ngOnInit(): void {
+		this.userService = this.userServ;
+
+		this.userService.getUser().subscribe(user => {
+			this.user$ = user;
+		})
+
+		this.questionService.getQuestions().subscribe(questions => {
+			this.allQuestions = questions;
+			this.flaggedQuestions = this.allQuestions.filter(question => question.flag > 0);
+		})
+	}
 
 }
