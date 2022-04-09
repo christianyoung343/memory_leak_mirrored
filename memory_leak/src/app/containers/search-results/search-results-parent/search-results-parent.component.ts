1355b35183b08b2a804cc8ac050da566711acf01
kody-kimberl <59657721+kody-kimberl@users.noreply.github.com>
2022-04-09 14:18:17 -0500

    Added cleaning of input

--- memory_leak/src/app/containers/search-results/search-results-parent/search-results-parent.component.ts
+++ memory_leak/src/app/containers/search-results/search-results-parent/search-results-parent.component.ts
@@ -21,34 +21,46 @@ export class SearchResultsParentComponent implements OnInit {
 	public keywords: Array<string> = [];
 
 	constructor(private qs: QuestionService, private router: Router, private http: HttpClient) {
-		this.searchForm = new FormGroup ({
+		this.searchForm = new FormGroup({
 			term: new FormControl(""),
 			inputAnswered: new FormControl(false)
-		  })
-		  http.get("assets/commonWords.txt", {responseType: 'text'}).subscribe(data => {
+		})
+		http.get("assets/commonWords.txt", { responseType: 'text' }).subscribe(data => {
 			this.keywords = data.split('\n');
-		  })
-	 }
+		})
+	}
 
 	ngOnInit(): void {
-		this.qs.getQuestions().subscribe( quests => {
+		this.qs.getQuestions().subscribe(quests => {
 			this.allQuestions = quests;
 		})
 	}
 
+	cleanQuestions(list: Array<string>): Array<string> {
+		let newList: Array<string> = [];
+		list.forEach(str => {
+			let newStr = "";
+			for (let i = 0; i < str.length; i++) {
+				if (str.charAt(i).match(RegExp("[a-zA-Z0-9]"))) {
+					newStr += str.charAt(i);
+				}
+			}
+			newList.push(newStr.toLowerCase());
+		})
+		return newList;
+	}
 
-	searchQuestions(searchTerm: { term:string, inputAnswered: Boolean}){
+	searchQuestions(searchTerm: { term: string, inputAnswered: Boolean }) {
 		let finalizedTerms: Array<string> = [];
-		let userTerm = searchTerm.term.split(' ');
-		userTerm.forEach( word => {
-			if(!this.keywords.includes(word) && !finalizedTerms.includes(word)){
+		let userTerm = this.cleanQuestions(searchTerm.term.split(' '));
+		userTerm.forEach(word => {
+			if (!this.keywords.includes(word) && !finalizedTerms.includes(word)) {
 				finalizedTerms.push(word);
 			}
 		});
-		console.log(finalizedTerms)
 		this.filteredQuestions = [];
-		for(let i = 0; i < this.allQuestions.length; ++i){
-			if(this.allQuestions[i].title.toLowerCase().includes(searchTerm.term.toLowerCase())){
+		for (let i = 0; i < this.allQuestions.length; ++i) {
+			if (this.allQuestions[i].title.toLowerCase().includes(searchTerm.term.toLowerCase())) {
 				this.filteredQuestions.push(this.allQuestions[i]);
 			}
 		}
