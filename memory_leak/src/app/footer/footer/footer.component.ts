a2b94771028f16664b9a26e05998507f3089d325
christianyoung343 <csy5@students.uwf.edu>
2022-04-22 02:59:42 +0100

    admin link shown only for admins

--- memory_leak/src/app/footer/footer/footer.component.ts
+++ memory_leak/src/app/footer/footer/footer.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { User } from 'src/models/user';
+
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+	@Input() user!: User;
 
-  constructor() { }
+	constructor(private userService: UserService) { }
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.userService.getUser().subscribe(user => {
+			if(user) {
+				this.user = user;
+			}
+		})
+	}
 }
